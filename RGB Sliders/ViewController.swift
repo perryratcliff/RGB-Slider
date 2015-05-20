e94c3de0e67d477d97b40e9926470109317a3d74
Perry Ratcliff <perryratcliff@Perrys-MBP.fios-router.home>
2015-05-19 17:14:39 -0700

    Initial Commit

---
+++ RGB Sliders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGB Sliders
+//
+//  Created by Perry Ratcliff on 5/19/15.
+//  Copyright (c) 2015 Perry Ratcliff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
